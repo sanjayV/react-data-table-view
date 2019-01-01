32bc1aff6df2ac47441948135df88cc90d8399b6
sanjay <skumarverma45@gmail.com>
2019-01-01 18:01:24 +0530

    added props types

--- projects/react-data-table-view/src/index.js
+++ projects/react-data-table-view/src/index.js
@@ -1,14 +1,8 @@
-import prop from 'prop-types';
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import './styles/table.css';
 
 class TableWrapper extends Component {
-    static propTypes = {
-        title: prop.string,
-    }
-    static defaultProps = {
-        title: 'Default title',
-    }
     render() {
         return (
             <div className="basic">
@@ -20,6 +14,8 @@ class TableWrapper extends Component {
                                 <th>#</th>
                                 <th>Test Header 1</th>
                                 <th>Test Header 2</th>
+                                <th>Test Header 3</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -27,11 +23,71 @@ class TableWrapper extends Component {
                                 <td>1</td>
                                 <td>Test Data Row 1 col 1</td>
                                 <td>Test Data Row 1 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
                             </tr>
                             <tr>
                                 <td>2</td>
                                 <td>Test Data Row 2 col 1</td>
                                 <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>7</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>8</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                                <td>Test Data Row 3</td>
+                                <td><button type="button">Edit</button></td>
                             </tr>
                         </tbody>
                     </table>
@@ -40,5 +96,34 @@ class TableWrapper extends Component {
         );
     }
 }
+
+TableWrapper.propTypes = {
+    /**
+     * Table heading
+     */
+    title: PropTypes.string,
+
+    /**
+     * Table theme
+     */
+    theme: PropTypes.string,
+
+    /**
+     * Adds borders on all sides of the table and cells.
+     */
+    bordered: PropTypes.bool,
+
+    /**
+     * Enable a hover state on table rows within a `<tbody>`.
+     */
+    hover: PropTypes.bool
+};
+
+TableWrapper.defaultProps = {
+    title: 'Default title',
+    theme: 'basic',
+    bordered: false,
+    hover: true
+};
 
 export default TableWrapper;
