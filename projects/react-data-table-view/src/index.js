17d9eb7c703609364191feb8376fa5e850b70a26
sanjay <skumarverma45@gmail.com>
2019-01-01 11:37:47 +0530

    updated some style of static table

--- projects/react-data-table-view/src/index.js
+++ projects/react-data-table-view/src/index.js
@@ -10,39 +10,34 @@ class TableWrapper extends Component {
         title: 'Default title',
     }
     render() {
-        return <div>
-            {this.props.title}
-            <table className="react-tables">
-                <thead>
-                    <tr>
-                        <td>
-                            Test Header 1
-                        </td>
-                        <td>
-                            Test Header 2
-                        </td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            Test Data Row 1 col 1
-                        </td>
-                        <td>
-                            Test Data Row 1 col 2
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Test Data Row 2 col 1
-                        </td>
-                        <td>
-                            Test Data Row 2 col 2
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>;
+        return (
+            <div className="basic">
+                <div className="table-responsive">
+                    <div className="table-title">{this.props.title}</div>
+                    <table className="react-tables">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Test Header 1</th>
+                                <th>Test Header 2</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Test Data Row 1 col 1</td>
+                                <td>Test Data Row 1 col 2</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Test Data Row 2 col 1</td>
+                                <td>Test Data Row 2 col 2</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import React, { Component } from 'react'
 
 class App extends Component {
     state = {loading: false}
-
+    /* <div className="App-heading App-flex">
+        <h2>Welcome to <span className="App-react">React</span></h2>
+    </div> */
     render() {
         return <div className="App">
-            <div className="App-heading App-flex">
-                <h2>Welcome to <span className="App-react">React</span></h2>
-            </div>
-            <DataTable
-              title="Test Data"
-            />
-            <div className="App-instructions App-flex">
-                <img className="App-logo" src={require('./react.svg')} />
-                <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
-            </div>
+            <DataTable title="Test Data"></DataTable>
         </div>
     }
 }
